9977041cfd95e4d10a67858de1c8ec64929df9a9
jotta-hop <yeyefortcheka@gmail.com>
2022-09-20 16:06:36 -0500

    added commit to index introdution api fetch

--- introduction-js/introduction-api-fetch/index.js
+++ introduction-js/introduction-api-fetch/index.js
@@ -18,20 +18,24 @@ function mostrar_usuarios (users) {
     let tbody = `<tbody>`
 
     for (let i = 0; i < users.length; i++) {
+        //Obtener la dirreción
+        let address = users[i].address.street
+        address += `, ${users[i].address.suite}`
+        address += `, ${users[i].address.city}`
+        // Variable que representa el cuerpo de la tabla
         tbody += `
             <tr>
             <td>${users[i].id}</td>
-            <td>${users.name}</td>
-            <td>${users.username}</td>
-            <td>${users.email}</td>
-            <td>${users.address}</td>
-            <td>${users.phone}</td>
-            <td>${users.website}</td>
-            
+            <td>${users[i].name}</td>
+            <td>${users[i].username}</td>
+            <td>${users[i].email}</td>
+            <td>${address}</td>
+            <td>${users[i].phone}</td>
+            <td>${users[i].website}</td>
             </tr>
         `
     }
-    tdoby += `</tbody>`
+    tbody += `</tbody>`
     tabla.innerHTML += tbody
 }  
     
