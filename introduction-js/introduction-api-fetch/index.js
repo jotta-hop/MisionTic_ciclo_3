79e2b678a962009bd93990f498e91cd17d48179d
jotta-hop <yeyefortcheka@gmail.com>
2022-09-19 23:14:33 -0500

    actualizacion introduction-api-fetch/index.js

--- introduction-js/introduction-api-fetch/index.js
+++ introduction-js/introduction-api-fetch/index.js
@@ -1,40 +1,46 @@
 const uri_users = 'https://jsonplaceholder.typicode.com/users'
 
 // Obtener los usuarios
-async function obtener_usurio(uri_users) {
+async function obtener_usurios(uri_users) {
     //Enviar petición de tipo Get 
     const resp = await fetch(uri_users)
-    console.log(resp)
+   
+    //console.log(resp)
     const data = await resp.json()
-    //console.log(data)
+    console.log(data)
     return data    
 }
 
 // Mostrar los ususarios
-function mostrar_usuarios (user) {
+function mostrar_usuarios (users) {
     // Referenciar tabla
     const tabla = document.getElementById('tabla')
-    const tbody = `<tbody>`
+    let tbody = `<tbody>`
 
-    for (let i = 0; i <users.length; i++) {
+    for (let i = 0; i < users.length; i++) {
         tbody += `
             <tr>
-            <td>${users.id}</td>
+            <td>${users[i].id}</td>
             <td>${users.name}</td>
             <td>${users.username}</td>
+            <td>${users.email}</td>
+            <td>${users.address}</td>
+            <td>${users.phone}</td>
+            <td>${users.website}</td>
             
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
             </tr>
         `
     }
     tdoby += `</tbody>`
     tabla.innerHTML += tbody
-}
+}  
     
     
 // Función pincipal
+async function main(){
+    const users = await obtener_usurios(uri_users)
+    mostrar_usuarios(users)
+}
+obtener_usurios(uri_users)
 
-obtener_usurio(uri_users)
+main()
